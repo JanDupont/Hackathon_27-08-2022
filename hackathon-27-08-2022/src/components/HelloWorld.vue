1b0322dbc0f7dd28671db25e2564a7a8d949f1b9
JanDupont <jan.dupont7@gmail.com>
2022-08-28 10:57:11 +0200

    first commit

---
+++ hackathon-27-08-2022/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+	<GraphlyD3 ref="graphly" />
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import { GraphlyD3 } from "@livereader/graphly-d3-vue";
+import "@livereader/graphly-d3-vue/style.css";
+
+const graphly = ref<any>(null);
+
+onMounted(() => {
+	const simulation = graphly.value?.simulation;
+	simulation.render({
+		nodes: [],
+		links: [],
+	});
+});
+</script>
+
+<style scoped></style>
