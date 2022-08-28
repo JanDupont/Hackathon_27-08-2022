e3d080a49892d9aa2d73fd32b8de2a6f7ac8adb8
JanDupont <jan.dupont7@gmail.com>
2022-08-28 13:41:30 +0200

    unfinished shape

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
-	<GraphlyD3 ref="graphly" />
+	<div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%">
+		<GraphlyD3 ref="graphly" />
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -7,14 +9,32 @@ import { ref, onMounted } from "vue";
 import { GraphlyD3 } from "@livereader/graphly-d3-vue";
 import "@livereader/graphly-d3-vue/style.css";
 
+import DroneTemplate from "../templates/drone";
+import { AnchorType, Graph, Node } from "@livereader/graphly-d3";
+
 const graphly = ref<any>(null);
+
+const graph = ref<Graph>({ nodes: [], links: [] });
+
+const testNode: Node = {
+	id: Math.random().toString(),
+	shape: { type: "drone", scale: 1 },
+	anchor: { type: AnchorType.Soft, x: 0, y: 0 },
+	payload: {
+		droneType: "quadcopter",
+		color: "white",
+		job: "observe",
+		battery: "50",
+		warning: true,
+		warningText: "Potentieller Waldbrand",
+	},
+};
 
 onMounted(() => {
 	const simulation = graphly.value?.simulation;
-	simulation.render({
-		nodes: [],
-		links: [],
-	});
+	simulation.templateStore.add("drone", DroneTemplate);
+	graph.value.nodes.push(testNode);
+	simulation.render(graph.value);
 });
 </script>
 
